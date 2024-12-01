2b9f32f80641e8fd443ca32057d543516123a9c1
vzteufem <marc.teufel@hama.de>
2024-12-01 06:58:54 +0100

    finished day 1

--- day1/day1.go
+++ day1/day1.go
@@ -1,17 +1,76 @@
 package day1
 
 import (
+	"aoc24/util"
 	"fmt"
+	"sort"
+	"strconv"
+	"strings"
 )
 
 func Task11() {
-	fmt.Println("\nDay 1, Part 1: XXX")
+	fmt.Println("\nDay 1, Part 1: Historian Hysteria")
 	fmt.Println("===============================================")
 	//lines := util.ReadFile("/day1/data/sample.txt")
+	//lines := util.ReadFile("/day1/data/1/input.txt")
+	first, second := prepare("/day1/data/1/input.txt")
+
+	fmt.Println("Result=", first)
+	fmt.Println("Result=", second)
+	sort.Slice(first, func(i, j int) bool { return first[i] < first[j] })
+	sort.Slice(second, func(i, j int) bool { return second[i] < second[j] })
+	fmt.Println("Result=", first)
+	fmt.Println("Result=", second)
+	total := 0
+	for idx, firstValue := range first {
+		secondValue := second[idx]
+		diff := 0
+		if firstValue > secondValue {
+			diff = firstValue - secondValue
+		} else {
+			diff = secondValue - firstValue
+		}
+		total = total + diff
+	}
+	fmt.Println("Result=", total)
+
 }
 
 func Task12() {
-	fmt.Println("\nDay 1, Part 2: XXX")
+	fmt.Println("\nDay 1, Part 2: Historian Hysteria")
 	fmt.Println("===============================================")
-	//lines := util.ReadFile("/day1/input.txt")
+	//first, second := prepare("/day1/data/2/sample.txt")
+	first, second := prepare("/day1/data/2/input.txt")
+
+	fmt.Println("First=", first)
+	fmt.Println("Second=", second)
+
+	dict := make(map[int]int)
+	for _, num := range second {
+		dict[num] = dict[num] + 1
+	}
+	fmt.Println("SecondDictionary=", dict)
+	total := 0
+	for _, value := range first {
+		total = total + dict[value]*value
+	}
+	fmt.Println("Result=", total)
+}
+
+func prepare(data string) ([]int, []int) {
+	lines := util.ReadFile(data)
+
+	first := make([]int, 0)
+	second := make([]int, 0)
+	var i int
+	for _, l := range lines {
+		words := strings.Fields(l)
+		i, _ = strconv.Atoi(words[0])
+		first = append(first, i)
+
+		i, _ = strconv.Atoi(words[1])
+		second = append(second, i)
+
+	}
+	return first, second
 }
--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
-import "aoc24/day1"
+import (
+	"aoc24/day1"
+)
 
 func main() {
-	day1.Task11()
-
+	//day1.Task11()
+	day1.Task12()
 }
